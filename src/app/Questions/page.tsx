f1be866f32fd5cc51780bd6de2489c6ef2853944
sam <sam@meech-ward.me>
2023-04-14 00:30:21 -0700

    add loading indicator to qurstions pages

--- src/app/Questions/page.tsx
+++ src/app/Questions/page.tsx
@@ -15,6 +15,8 @@ const SurveyForm: React.FC = () => {
 
   const { userId, isLoading, role } = useContext(RoleContext);
 
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
   const project = searchParams.get("project");
 
   const [questions, setQuestions] = useState<Question[]>([
@@ -43,7 +45,7 @@ const SurveyForm: React.FC = () => {
 
   const goBack = () => {
     router.back();
-  }
+  };
 
   const updateAnswer = (index: number, value: number) => {
     const updatedQuestions = [...questions];
@@ -83,6 +85,7 @@ const SurveyForm: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsSubmitting(true);
     console.log({ questions, comments, project });
     const result = await fetch("/api/surveys", {
       method: "POST",
@@ -94,20 +97,21 @@ const SurveyForm: React.FC = () => {
     const data = await result.json();
     if (data.error) {
       alert(data.error);
-      return
-    } 
+      setIsSubmitting(false);
+      return;
+    }
     if (result.ok) {
       router.push("/Success");
     }
   };
 
   if (isLoading) {
-    return <></>
+    return <></>;
   }
 
   if (!project) {
     router.push("/");
-    return <></>
+    return <></>;
   }
 
   if (!userId || !role) {
@@ -120,67 +124,97 @@ const SurveyForm: React.FC = () => {
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
 
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div className="flex items-center space-x-5">
-              <div className="h-14 w-14 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl font-medium hover:cursor-pointer" onClick={goBack}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M10 19l-7-7m0 0l7-7m-7 7h18"
-                  />
-                </svg>
+        {isSubmitting ? (
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="max-w-md mx-auto">
+              <div className="flex items-center space-x-5">
+                <div className="h-14 w-14 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl font-medium">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 animate-spin"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
+                  </svg>
+                </div>
+                <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
+                  <h2 className="leading-relaxed">Submitting...</h2>
+                </div>
               </div>
-              <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                <h2 className="leading-relaxed">Survey</h2>
-                <p className="text-sm text-gray-500 font-normal leading-relaxed">
-                  Please rate the following questions
+            </div>
+          </div>
+        ) : (
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="max-w-md mx-auto">
+              <div className="flex items-center space-x-5">
+                <div
+                  className="h-14 w-14 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl font-medium hover:cursor-pointer"
+                  onClick={goBack}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                    />
+                  </svg>
+                </div>
+                <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
+                  <h2 className="leading-relaxed">Survey</h2>
+                  <p className="text-sm text-gray-500 font-normal leading-relaxed">
+                    Please rate the following questions
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-between pt-6">
+                <p className="font-semibold text-center mb-6 text-black">
+                  1: worst
+                </p>
+                <p className="font-semibold text-center mb-6 text-black">
+                  5: best
                 </p>
               </div>
             </div>
-              <div className="flex justify-between pt-6">
-            <p className="font-semibold text-center mb-6 text-black">
-              1: worst
-            </p>
-            <p className="font-semibold text-center mb-6 text-black">
-              5: best
-            </p>
-          </div>
-          </div>
-          
-          <form onSubmit={handleSubmit}>
-            {renderQuestions()}
-            <div className="mb-5">
-              <label
-                htmlFor="comments"
-                className="block mb-2 text-gray-700 text-sm font-medium"
+
+            <form onSubmit={handleSubmit}>
+              {renderQuestions()}
+              <div className="mb-5">
+                <label
+                  htmlFor="comments"
+                  className="block mb-2 text-gray-700 text-sm font-medium"
+                >
+                  Optional comments
+                </label>
+                <textarea
+                  id="comments"
+                  className="w-full p-2 border border-gray-300 rounded-md text-black"
+                  value={comments}
+                  onChange={(e) => setComments(e.target.value)}
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md"
               >
-                Optional comments
-              </label>
-              <textarea
-                id="comments"
-                className="w-full p-2 border border-gray-300 rounded-md text-black"
-                value={comments}
-                onChange={(e) => setComments(e.target.value)}
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );

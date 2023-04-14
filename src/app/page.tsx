02b22df5673e0d7fa0b79d27d142a41032fcccdb
sam <sam@meech-ward.me>
2023-04-14 12:08:31 -0700

    update names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,19 +26,22 @@ export default function Home() {
 
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 min-w-">
           <button onClick={() => router.push("/Questions?project=1")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
-            Prancer
+            Buddytree
           </button>
           <button onClick={() => router.push("/Questions?project=2")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
-            Dancer
+            Cibo
           </button>
           <button onClick={() => router.push("/Questions?project=3")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
-            Blitzen
+            SISA Eneergy
           </button>
           <button onClick={() => router.push("/Questions?project=4")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
-            Comet
+            Innota
           </button>
           <button onClick={() => router.push("/Questions?project=5")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
-            Snarfle
+            VIO
+          </button>
+          <button onClick={() => router.push("/Questions?project=6")} className="w-full py-3 mt-10 bg-blue-600 text-white text-center font-medium rounded-md p-8">
+            Chikadee
           </button>
         </div>
       </div>
